454d2d10a649e4e6353f8fb59a2489d9fa544dde
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-10-10 10:29:17 +0200

    small changes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useContext } from "react";
-import CounterText from "./CounterText";
 
 const LandingPage = () => {
     const {count} = useContext(CounterText);
